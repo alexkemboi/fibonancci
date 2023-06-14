b61e149cbbcbd00c6b001ad9e03e879c24f0700e
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-14 16:32:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Fibonacci{
+         public static int[] ComputeSeries(int length){
+         int firstNumber = 0;
+        int secondNumber = 1;
+        int[] series = new int[length];
+        series[0] = firstNumber;
+        series[1] = secondNumber;
+        for(int i=2;i<length;i++){
+            series[i] = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = series[i];
+        }
+        return series;
+    }
+}
+public class Program{
+    public static void Main(string [] args){
+        int[] series = Fibonacci.ComputeSeries(5);
+        foreach(int number in series){
+          Console.Write(number+" ");
+        }
+    }
+}
